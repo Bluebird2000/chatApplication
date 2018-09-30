8ab2c08f7b0fe89ff0f67b8c90a9288448cf95ce
Bluebird2000 <olamilekanahmad7@gmail.com>
2018-09-30 23:25:23 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import http from 'http';
+import path from 'path';
+import socketIO from 'socket.io';
+import dotenv from 'dotenv';
+dotenv.config();
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+var server = http.createServer(app);
+app.use(express.static(publicPath));
+const io = socketIO(server);
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(process.env.PORT, () => console.log(`Server now listening on port ${process.env.PORT}`));
